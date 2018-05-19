a6b5b8193658be3b43b7f44e69ffa358ec189d2b
Vivek Patel <therealvivek9064@gmail.com>
2018-05-18 23:16:22 -0700

    Wallpaper threads are confirmed to be working
    Ran some tests with random data to make sure the wallpaper was indeed being redrawn every minute. Gonna move on to parsing time series tomorrow.

--- PaperShares/app/src/main/java/com/horizon/vpatel/papershares/StockGraphWallpaperService.java
+++ PaperShares/app/src/main/java/com/horizon/vpatel/papershares/StockGraphWallpaperService.java
@@ -3,14 +3,19 @@ package com.horizon.vpatel.papershares;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorSpace;
 import android.graphics.Paint;
+import android.graphics.Rect;
 import android.service.wallpaper.WallpaperService;
 import android.view.SurfaceHolder;
 import android.os.Handler;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.GridLabelRenderer;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
+
+import java.util.Random;
 
 public class StockGraphWallpaperService extends WallpaperService{
 
@@ -19,9 +24,11 @@ public class StockGraphWallpaperService extends WallpaperService{
     int Drawspeed = 60;
     Context mcontext;
 
+    Random r;
+
 
     //The information for the graph
-    private GraphView toDraw = new GraphView(getBaseContext());
+    private GraphView toDraw;
 
     private DataPoint pricesToDraw[] = new DataPoint[60];
 
@@ -31,6 +38,30 @@ public class StockGraphWallpaperService extends WallpaperService{
     public void onCreate()
     {
         super.onCreate();
+
+        //Set up some properties of the graph
+        currentGraphSeries.setThickness(8);
+        currentGraphSeries.setDataPointsRadius(2.0f);
+        currentGraphSeries.setDrawDataPoints(true);
+        currentGraphSeries.setAnimated(true);
+
+        toDraw = new GraphView(getApplicationContext()); //Instantiate
+        toDraw.getGridLabelRenderer().setHorizontalLabelsVisible(false);
+        toDraw.getGridLabelRenderer().setVerticalLabelsVisible(false);
+        toDraw.getGridLabelRenderer().setGridStyle(GridLabelRenderer.GridStyle.NONE);
+        toDraw.getViewport().setMinX(0); toDraw.getViewport().setMaxX(59);
+        toDraw.getViewport().setScalable(true);
+
+        toDraw.getGridLabelRenderer().setLabelsSpace(0);
+
+
+        r = new Random();
+        for (int i = 0; i < pricesToDraw.length; i++)
+        {
+            pricesToDraw[i] = new DataPoint(i, r.nextDouble() * 69);
+            currentGraphSeries.appendData(pricesToDraw[i], false, 60);
+        }
+        toDraw.addSeries(currentGraphSeries);
     }
 
     public void onDestroy()
@@ -43,7 +74,6 @@ public class StockGraphWallpaperService extends WallpaperService{
     public Engine onCreateEngine() {
         return new StockWallpaperEngine();
     }
-
 
     public class StockWallpaperEngine extends Engine
     {
@@ -103,11 +133,23 @@ public class StockGraphWallpaperService extends WallpaperService{
                 if (canvas != null) {
 
                     //Time to refresh the data
-
-
                     //Look at this graph
+
+                    toDraw.setBackgroundColor(getColor(R.color.colorPrimary));
+
+                    currentGraphSeries.setColor(getColor(R.color.colorAccent));
+
+
+                    for (int i = 0; i < pricesToDraw.length; i++)
+                    {
+                        pricesToDraw[i] = new DataPoint(i, r.nextDouble() * 69);
+                    }
+                    currentGraphSeries.resetData(pricesToDraw);
+
+
+                    toDraw.measure(canvas.getWidth(), canvas.getHeight());
+                    toDraw.layout(0, 0, canvas.getWidth(), canvas.getHeight());
                     toDraw.addSeries(currentGraphSeries);
-                    toDraw.measure(1440, 2560);
                     toDraw.draw(canvas);
                     }
             } finally {
